fcd733afb886215ac11e76f606b88dd0e5674ce6
santiago <108821911+SantiZapata1@users.noreply.github.com>
2023-08-10 10:26:21 -0300

    Add files via upload

---
+++ Programacion_2/src/cifras.java
@@ -0,0 +1,10 @@
+public class cifras {
+
+    public static void main(String args[]) {
+
+    }
+
+
+    //hacerlo dividiendo x10 e ir contando hasta no poderublic static int cantCifras2(int valor) {
+
+}
---
+++ Programacion_2/src/repaso.java
@@ -0,0 +1,17 @@
+public class repaso {
+
+    //procedimiento principal
+    public static void main(String args[]){
+
+        int resultado = suma(3,5);
+
+    }
+
+    //funcion suma
+    public static int suma(int a, int b){
+        return a+b;
+    }
+
+}
+
+//entender paso de parametro por valor/referencia
